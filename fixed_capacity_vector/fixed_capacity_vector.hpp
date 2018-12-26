24c8ed9cd96d742f3a673d0b20eca21d60e87354
Yu Xiang <tonyxiang@live.nl>
2018-12-26 12:56:18 +0100

    finish class

--- fixed_capacity_vector/fixed_capacity_vector.hpp
+++ fixed_capacity_vector/fixed_capacity_vector.hpp
@@ -2,7 +2,6 @@
 
 #include <memory>
 
-
 template<class T>
 class FixedCapacityVector {
 private:
@@ -10,19 +9,30 @@ private:
 public:
 	FixedCapacityVector(FixedCapacityVector const&) = delete;
 	FixedCapacityVector& operator=(FixedCapacityVector const&) = delete;
-	FixedCapacityVector(FixedCapacityVector &&) = delete;
-	FixedCapacityVector& operator=(FixedCapacityVector &&) = delete;
-
 	FixedCapacityVector(size_t capacity = 0):
 		capacity_{ capacity },
 		data_{ std::make_unique<StorageType[]>(capacity) }
 	{ }
-
-	template<class... Args>
-	T& emplace_back(Args&&... args)
+	~FixedCapacityVector()
 	{
-		new (&data_[size_]) T{ std::forward<Args>(args)... };
+		for (size_t i = 0; i < size_; i++)
+			reinterpret_cast<T&>(data_[i]).~T();
 	}
+	template<class... Args>
+	T& emplace_back(Args&&... args) 
+	{
+		if (size_ == capacity_)
+			throw std::bad_alloc{};
+		T* ptr = new (&data_[size_]) T{ std::forward<Args>(args)... };
+		size_++;
+		return *ptr;
+	}
+	T& operator[](size_t i) 
+	{ return reinterpret_cast<T&>(data_[i]); }
+	T const& operator[](size_t i) const 
+	{ return reinterpret_cast<T const&>(data_[i]); }
+	size_t size() const { return size_; }
+	size_t capacity() const { return capacity_; }
 private:
 	size_t const capacity_;
 	std::unique_ptr<StorageType[]> const data_;
