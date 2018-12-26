19ccdd19d4e1ba216497b53ad1cfc69ae0448f95
Yu Xiang <tonyxiang@live.nl>
2018-12-26 11:56:11 +0100

    start to write class

--- fixed_capacity_vector/fixed_capacity_vector.hpp
+++ fixed_capacity_vector/fixed_capacity_vector.hpp
@@ -1,1 +1,20 @@
 #pragma once
+
+#include <memory>
+
+
+template<class T>
+class FixedCapacityVector {
+private:
+	using StorageType = std::aligned_storage_t<sizeof(T), alignof(T)>;
+public:
+	FixedCapacityVector(FixedCapacityVector const&) = delete;
+	FixedCapacityVector& operator=(FixedCapacityVector const&) = delete;
+	FixedCapacityVector(FixedCapacityVector &&) = delete;
+	FixedCapacityVector& operator=(FixedCapacityVector &&) = delete;
+
+private:
+	std::unique_ptr<T[]> data_;
+	size_t size_;
+	size_t const capacity_;
+};
