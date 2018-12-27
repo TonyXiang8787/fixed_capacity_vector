048b233c0ebb7e5a5aff1471ece426e86ef03b44
Yu Xiang <tonyxiang@live.nl>
2018-12-27 10:18:23 +0100

    test nested size

--- fixed_capacity_vector/example.cpp
+++ fixed_capacity_vector/example.cpp
@@ -1,6 +1,9 @@
 #include "test_func.hpp"
+#include "nested_struct.hpp"
 
 int main()
 {
 	test_vector();
+	VSizeT v_size;
+	((VSize<DType::kInt8>&)v_size).size = 5;
 }
--- fixed_capacity_vector/nested_struct.hpp
+++ fixed_capacity_vector/nested_struct.hpp
@@ -12,6 +12,14 @@ enum class DType : int32_t {
 	kInt,
 	kInt16
 };
+inline constexpr DType prev(DType dtype) {
+	return static_cast<DType>(
+		static_cast<std::underlying_type_t<DType>>(dtype) - 1); }
+inline constexpr DType next(DType dtype) {
+	return static_cast<DType>(
+		static_cast<std::underlying_type_t<DType>>(dtype) + 1);
+}
+
 constexpr std::array<DType, 5> data_type_arr {
 	DType::kD,
 	DType::kInt32,
@@ -20,25 +28,28 @@ constexpr std::array<DType, 5> data_type_arr {
 	DType::kInt16
 };
 
-template<DType dtype, class = void> struct enum_map;
-template<> struct enum_map<DType::kD> {
+template<DType dtype, class = void> struct EnumMap;
+template<> struct EnumMap<DType::kD> {
 	using type = double; };
-template<> struct enum_map<DType::kInt8> {
+template<> struct EnumMap<DType::kInt8> {
 	using type = int8_t; };
-template<> struct enum_map<DType::kInt16> {
+template<> struct EnumMap<DType::kInt16> {
 	using type = int16_t; };
 template<DType dtype> 
-struct enum_map<dtype, 
+struct EnumMap<dtype, 
 	std::enable_if_t<dtype == DType::kInt32 || dtype == DType::kInt>>
 {
 	using type = int32_t;
 };
+template<DType dtype>
+using EnumMapT = typename EnumMap<dtype>::type;
 
-template<size_t N, class = void> struct v_size;
-template<size_t N> 
-struct v_size<N, std::enable_if_t<N == 0>> { size_t size; };
-template<size_t N>
-struct v_size<N, std::enable_if_t<(N > 0)>> : v_size<N - 1> {
+template<DType dtype, class = void> struct VSize;
+template<DType dtype>
+struct VSize<dtype, std::enable_if_t<dtype == data_type_arr.front()>> { size_t size; };
+template<DType dtype>
+struct VSize<dtype, std::enable_if_t<dtype != data_type_arr.front()>> : 
+	VSize<prev(dtype)> {
 	size_t size;
 };
-using v_size_t = v_size<data_type_arr.size() - 1>;
+using VSizeT = VSize<data_type_arr.back()>;
